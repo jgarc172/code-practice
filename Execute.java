ba311de4345a0c7c34cbe638648f6e7a6f07a9a9
jgarc172 <jgarc172@mail.ccsf.edu>
2017-10-15 09:04:24 -0700

    fundamentals

---
+++ Execute.java
@@ -0,0 +1,5 @@
+class Execute {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ OneFunction.java
@@ -0,0 +1,10 @@
+class OneFunction{
+
+    public void nothing() {
+        
+    }
+    public static void main(String[] args) {
+        OneFunction object = new OneFunction();
+        object.nothing();
+    }
+}
